16b0b951afce449f254e05ea9aff02b653a11c60
TKdevlop <34345518+TKdevlop@users.noreply.github.com>
2018-03-27 18:05:30 +0530

    Final change in Todo API

--- server/models/todo.js
+++ server/models/todo.js
@@ -1,21 +1,20 @@
-let mongoose = require("mongoose");
-let Todo = mongoose.model("Todo", {
-    text: { 
-      type: "String",
-      required:true,
-      minlength:1,
-      trim:true
-    },
-    completed: {
-      type: "Boolean",
-      default:false
-    },
-    completedAt: {
-      type: "Number",
-      default:null
-    }
+var mongoose = require('mongoose');
 
-  });
-  module.exports = {
-      Todo
+var Todo = mongoose.model('Todo', {
+  text: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  completedAt: {
+    type: Number,
+    default: null
   }
+});
+
+module.exports = {Todo};
